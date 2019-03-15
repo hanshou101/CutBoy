6e88a80a149be4f528fe16949f9150539086f07e
NewYoung <657851574@qq.com>
2019-03-15 16:57:55 +0800

    新增调试模式

--- index.js
+++ index.js
@@ -11,11 +11,13 @@
     }
 }(function () {
     /**
-     * debug 调试模式
      * pageImage 设计稿图片
-     * pageMat 设计稿图片数据
      * $element 待检测元素
      * $cover 覆盖元素
+     * $result 匹配结果示意画布
+     * debug 调试模式
+     * $debug 调试元素
+     * $dialog 弹出框
      */
     function CutBoy(params){
         var self = this;
@@ -25,16 +27,106 @@
         self.pageImage.src = params.pageImageSrc;
         self.pageImage.onload = function(){
             Module['onRuntimeInitialized'] = function() {//保证opecv wasm 编译完成
-                self.pageMat = cv.imread(self.pageImage);
                 self._events();
             };
         }
 
         if(self.debug){
-            html2canvas(document.body).then(function(canvas){
-                self.downlaodCanvas(canvas,'screen');
+            self._debug();
+        }
+    }
+
+    /**
+     * 调试模式
+     */
+    CutBoy.prototype._debug = function(){
+        var self = this;
+
+        if(!self.$debug){
+            //调试工具栏
+            self.$debug = document.createElement('div');
+            self.$debug.id = 'cut-boy-debug';
+            document.body.appendChild(self.$debug);
+            self.$debug.innerHTML = '<button func="result">匹配结果</button><button func="compare">差异比较</button>';
+
+            //弹出框
+            self.$dialog = document.createElement('div');
+            self.$dialog.id = 'cut-boy-dialog';
+            self.$dialog.style.display = 'none';
+            document.body.appendChild(self.$dialog);
+            self.$dialog.innerHTML = '<div></div>';
+
+            self.$debug.addEventListener('click',function(e){
+                var $target = e.target;
+
+                var $current = document.querySelector('#cut-boy-debug .cbd-current');
+                if($current){
+                    $current.classList.remove('cbd-current');
+                }
+
+                $target.classList.add('cbd-current');
+
+                var func = $target.getAttribute('func');
+                switch(func){
+                    case 'compare':
+                        self._showCompareResult();
+                        break;
+                    case 'result':
+                        self._showMatchingResult();
+                        break;
+                    default:
+                        break;
+                }
+            });
+
+            self.$dialog.addEventListener('click',function(){
+                self.$dialog.style.display = 'none';
             });
         }
+    }
+
+    /**
+     * 显示匹配结果
+     */
+    CutBoy.prototype._showMatchingResult = function(){
+        var self = this;
+
+        if(self.$result){
+            self.$dialog.innerHTML = '<div></div>';//初始化
+
+            var $container = document.querySelector('#cut-boy-dialog>div');
+            var img1 = new Image();
+            img1.src = self.$result.toDataURL();
+            $container.appendChild(img1);
+            var img2 = new Image();
+            $container.appendChild(img2);
+
+            self.$dialog.style.display = 'block';
+        }
+    }
+
+    /**
+     * 显示差异比较
+     */
+    CutBoy.prototype._showCompareResult = function(){
+        var self = this;
+        self.$dialog.innerHTML = '<div></div>';//初始化
+
+        self.$dialog.style.display = 'none';
+        self.$debug.style.display = 'none';
+
+        html2canvas(document.body).then(function(canvas){
+            self.$dialog.style.display = 'block';
+            self.$debug.style.display = 'block';
+
+            var $container = document.querySelector('#cut-boy-dialog>div');
+            var img1 = new Image();
+            img1.src = canvas.toDataURL();
+            $container.appendChild(img1);
+            $container.appendChild(self.pageImage);
+
+            self.$dialog.style.display = 'block';
+        });
     }
 
     /**
@@ -44,8 +136,9 @@
         var self = this;
 
         document.addEventListener('mousedown',function(e){
-            if(!self.$element){
-                self._cover(e.target);
+            var $target = e.target;
+            if(!self.$element&&(!self.$debug||!self.$debug.contains($target))&&(!self.$dialog||!self.$dialog.contains($target))){
+                self._cover($target);
             }
         });
 
@@ -110,55 +203,12 @@
     }
 
     /**
-     * canvas转换为blob
-     */
-    CutBoy.prototype.canvasToBlob = function(canvas,type){
-        var dataurl = canvas.toDataURL(type);
-        return this.dataURLtoBlob(dataurl);
-    }
-
-    /**
-     * dataurl转换为blob
-     */
-    CutBoy.prototype.dataURLtoBlob = function(dataurl) {
-        var arr = dataurl.split(','),
-            mime = arr[0].match(/:(.*?);/)[1];
-        var u8arr = this.dataURLtoUint8(dataurl);
-        return new Blob([u8arr], {type:mime});
-    }
-
-    /**
-     * dataurl转换为uint8
-     */
-    CutBoy.prototype.dataURLtoUint8 = function (dataurl) {
-        var arr = dataurl.split(','),
-            bstr = atob(arr[1]),
-            n = bstr.length,
-            u8arr = new Uint8Array(n);
-        while (n--) {
-            u8arr[n] = bstr.charCodeAt(n);
-        }
-        return u8arr;
-    }
-
-    /**
-     * 下载canvas画布
-     */
-    CutBoy.prototype.downlaodCanvas = function($canvas,name){
-        //下载
-        var aLink = document.createElement('a');
-        aLink.download = name+'.jpg';
-        var blob = this.canvasToBlob($canvas,'image/jpg');
-        aLink.href = URL.createObjectURL(blob);
-        var evt = new MouseEvent('click');//生出鼠标事件
-        aLink.dispatchEvent(evt);//触发鼠标事件
-    }
-
-    /**
      * 匹配
      */
     CutBoy.prototype._matching = function($temp){
+        var self = this;
         var templ = cv.imread($temp);
+        var src = cv.imread(self.pageImage);
 
         var dst = new cv.Mat();
         var mask = new cv.Mat();
@@ -175,22 +225,20 @@
          * cv.TM_CCOEFF 相关性系数匹配方法，该方法使用源图像与其均值的差、模版与其均值的差二者之间的相关性进行匹配，正值表示匹配的结果较好，负值表示匹配的结果较差；匹配值越大，匹配效果越好；
          * cv.TM_CCOEFF_NORMED 归一化相关性系数匹配方法。
          */
-        cv.matchTemplate(this.pageMat, templ, dst, cv.TM_CCOEFF_NORMED, mask);
+        cv.matchTemplate(src, templ, dst, cv.TM_CCOEFF_NORMED, mask);
 
         var result = cv.minMaxLoc(dst, mask);
         var position = result.maxLoc;
 
-        if(this.debug){
+        if(self.debug){
             //显示匹配结果
-            var $show = document.createElement('canvas');
-            $show.width = this.pageImage.width;
-            $show.height = this.pageImage.height;
+            self.$result = document.createElement('canvas');
+            self.$result.width = self.pageImage.width;
+            self.$result.height = self.pageImage.height;
             var color = new cv.Scalar(255, 0, 0, 255);
             var point = new cv.Point(position.x + templ.cols, position.y + templ.rows);
-            cv.rectangle(this.pageMat, position, point, color, 2, cv.LINE_8, 0);
-            cv.imshow($show, this.pageMat);
-
-            this.downlaodCanvas($show,'matching');
+            cv.rectangle(src, position, point, color, 2, cv.LINE_8, 0);
+            cv.imshow(self.$result, src);
         }
 
         dst.delete();
